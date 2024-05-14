e4d3d276929595dae8c6134f51450192989d3d5d
alonzo-bachelier <alonzo.bachelier@laplateforme.io>
2024-05-14 15:45:41 +0200

    création de la fonction postTask qui permet d'ajouter du contenu au fichier json

--- Jour2/routes.js
+++ Jour2/routes.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
-
+const tasksJson = JSON.parse(fs.readFileSync("./tasks.json"));
 function handleRequest(req, res) {
     if (req.url === "/tasks" && req.method === "GET") {
         getAllTasks(req, res);
+    } else if (req.url === "/tasks" && req.method === "POST") {
+        postTask(req, res);
     } else {
         defaultResponse(req, res);
     }
@@ -18,6 +20,36 @@ function getAllTasks(req, res) {
         res.end("Internal Server Error");
         console.error(error);
     }
+}
+
+function postTask(req, res) {
+    let body = "";
+
+    req.on("data", chunk => {
+        body += chunk.toString();
+    });
+
+    req.on("end", () => {
+        try {
+            const newTask = JSON.parse(body);
+            const tasks = JSON.parse(fs.readFileSync("./tasks.json"));
+
+            newTask.id = tasks.tasks.length
+                ? tasks.tasks[tasks.tasks.length - 1].id + 1
+                : 1;
+
+            tasks.tasks.push(newTask);
+
+            fs.writeFileSync("./tasks.json", JSON.stringify(tasks, null, 2));
+
+            res.writeHead(201, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(newTask));
+        } catch (error) {
+            res.writeHead(400, { "Content-Type": "text/plain" });
+            res.end("Invalid JSON");
+            console.error(error);
+        }
+    });
 }
 
 function defaultResponse(req, res) {
